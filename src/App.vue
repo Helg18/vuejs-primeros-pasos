d1154c3ab9cc8b03cc9d1d87ab506f2a9cf1d940
Henry Leon <helg18@gmail.com>
2018-05-09 20:28:23 -0400

    Fixed Url

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" height="64px">
+    <img src="https://helg18.github.io/vuejs-primeros-pasos/assets/logo.png" height="64px">
     <h1>{{ titulo }}</h1>
     <h2>{{ listArtists }}</h2>
     <select name="country" id="country" v-model="selectedCountry">
