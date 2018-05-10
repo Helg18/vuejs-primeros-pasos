e674af873486cf6e38f67a132c4075c43ce551a1
Henry Leon <helg18@gmail.com>
2018-05-09 20:33:13 -0400

    Redo dist folder

--- src/api/lastfm.js
+++ src/api/lastfm.js
@@ -3,5 +3,5 @@ export default {
 	apiKey:  "def60ea4faf48553f26dead4ffb5d774",
 	secret:  "c6d43f589c0180c06e3a7f8964b4c056",
 	registeredTo: "helg18",
-	urlBase: "http://ws.audioscrobbler.com/2.0/"
+	urlBase: "https://ws.audioscrobbler.com/2.0/"
 }
